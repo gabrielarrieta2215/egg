d66958580f551ecff1d8013e6ed182fecbeac78d
gabi arrieta <garrieta845@gmail.com>
2023-04-10 20:40:10 -0300

    primer commit

---
+++ src/javaintro/JAVAINTRO.java
@@ -0,0 +1,36 @@
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaintro;
+
+/**
+ *
+ * @author User
+ */
+public class JAVAINTRO {
+
+    /**System.out.println("Hola Mundo");
+
+
+     * @param args the command line arguments
+    */
+        // TODO code application logic here
+  public static void main(String[] args) {
+
+	int num = 10;
+    
+	System.out.println("La variable tiene el valor de: " + num);
+    
+	System.out.print("Hola");
+	System.out.print("Mundo");
+
+	}
+
+  
+
+ }
+    
